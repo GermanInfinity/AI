38ab0f857a114d2159ca8f15665c04ce9e817390
David <farfidan@gmail.com>
2019-11-10 16:16:12 -0500

    Following Artist.

---
+++ GA/Artist_individual.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 15:58:56 2019
+
+@author: ugoslight
+"""
+
+from graphics import *
+#win = GraphWin()
--- GA/GA-Traffic.py
+++ GA/GA-Traffic.py
@@ -14,7 +14,35 @@ Created on Sat Nov  9 12:18:31 2019
 #cull population
 
 #Individual - ["Go", "Wait", "Stop", "Go", "Wait"]
-from itertools import combinations
+import random
 
-colors = ["Go", "Wait", "Stop"]
-a = list(combinations(colors, 5))
+def mutation(individual, signs):
+    pos = random.randint(0,len(individual)-1)
+    print (pos)
+    sign = random.choice(signs)
+    individual[pos] = sign
+    return individual
+
+def cross_over(individual1, individual2):
+    cutoff = random.randint(0,4)
+    first_half = random.choice([individual1, individual2])
+    if first_half == individual1: sec_half = individual2
+    if first_half == individual2: sec_half = individual1
+    child = first_half[0:cutoff] + sec_half[cutoff:len(individual1)]
+    return child
+        
+signs = ["Go", "Wait", "Stop"]
+def make_pool(pop_count, signs):
+    count = 0 
+    pool = []
+    while count < pop_count: 
+        one_indi = []
+        for idx in range(5):
+            a = random.choice(signs)
+            one_indi.append(a)
+        pool.append(one_indi)
+        count += 1
+
+    return pool
+        
+    
