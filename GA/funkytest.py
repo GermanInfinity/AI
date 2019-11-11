6fd6a77f696a946531a60c1c04e4e8ee00ec4540
David <farfidan@gmail.com>
2019-11-11 16:16:37 -0500

    Prework. Module Simulation commences.

---
+++ GA/funkytest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 16:34:39 2019
+
+@author: ugoslight
+"""
+a = 4
+print (a)
--- GA/out.py
+++ GA/out.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 10 15:58:56 2019
-
-@author: ugoslight
-"""
-
 from graphics import *
-
 win = GraphWin('My Circle', 100, 100)
 c = Circle(Point(50,50), 10)
 ca = Circle(Point(55,75), 10)
 c.draw(win)
 ca.draw(win)
 win.getMouse()
-win.close()
+win.close() 
+main()
