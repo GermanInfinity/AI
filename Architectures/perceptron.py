3026cb55b2c55f595de8e8636e371e1e39d2735b
David <farfidan@gmail.com>
2020-04-19 13:09:59 -0400

    Created perceptron

--- Architectures/perceptron.py
+++ Architectures/perceptron.py
@@ -1,16 +1,22 @@
+"""
+Description : Implementation of simple perceptron
+"""
 import torch 
+import torch.nn as nn
 
 class Perceptron():
-	def __init__(self):
-		self.weights = random.randint#tensor
-		self.bias = 
+    def __init__(self, row, width):
+        self.weights = torch.rand((row, width))
+        self.bias = torch.rand((row, width))
 
-	def forward(input):
-        out = self.weight * input 
-	    out += self.bias
-	    out = sigmoid(out)
-		return out
+    def forward(self, input):
+        out = self.weights * input
+        out += self.bias
+        sig = nn.LogSigmoid()
+        out = sig(out)
+        return out
 
 
 if __name__ == '__main__':
-	
+    neuron = Perceptron(3, 5)
+    print (neuron.forward(2))
