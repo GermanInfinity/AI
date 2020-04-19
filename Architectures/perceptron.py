83e692e0769a97ae2ca379597f15bc98b00c9784
David <farfidan@gmail.com>
2020-04-19 05:01:29 -0400

    Archs added

---
+++ Architectures/perceptron.py
@@ -0,0 +1,16 @@
+import torch 
+
+class Perceptron():
+	def __init__(self):
+		self.weights = random.randint#tensor
+		self.bias = 
+
+	def forward(input):
+        out = self.weight * input 
+	    out += self.bias
+	    out = sigmoid(out)
+		return out
+
+
+if __name__ == '__main__':
+	
