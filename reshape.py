04cf4f826a5e8fb27068bd2b18e89a14d4f1ede3
David <farfidan@gmail.com>
2020-05-28 14:53:54 -0400

    Started GAN Project

--- reshape.py
+++ reshape.py
@@ -2,10 +2,10 @@ import os
 from PIL import Image
 
 count = 0
-new_width  = 224
-new_height = 224
+new_width  = 257
+new_height = 257
 #direc = "./fruit_data/train/oranges"
-direc = "../imagesCelline/MDA-MB-468/"
+direc = "../imagesCelline/images2/train/HC11"
 
 for filename in os.listdir(direc):
 	if filename.endswith(".png") or filename.endswith(".jpg"):
